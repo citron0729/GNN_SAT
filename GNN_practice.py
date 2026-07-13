e2cc1e91e90d052fd333863061482e8eb164e148
Yuzuki NOJI <yudukinoji2003@gmail.com>
2026-07-13 18:22:28 +0900

    GNNで初めての練習問題を解いたよ

--- GNN_practice.py
+++ GNN_practice.py
@@ -24,14 +24,29 @@ for node1, node2 in edges:
 for node in nodes:
     print(node, "の隣接ノード:", adjacency[node])
 
+# 隣接ノード数を表示する
 for node in nodes:
     print(node, "の隣接ノード数", len(adjacency[node]))
 
-    node_features = {
+# 最も次数の大きいノードを表示する
+max_degree = 0
+max_node = ""
+
+for node in nodes:
+    degree = len(adjacency[node])
+
+    if degree > max_degree:
+        max_degree = degree
+        max_node = node
+    
+print ("最も次数が大きいノード:", max_node)
+
+node_features = {
     "A": 1.0,
     "B": 2.0,
     "C": 3.0,
     "D": 4.0,
+    "E": 5.0,
 }
 
 updated_features = {}
@@ -43,7 +58,7 @@ for node in nodes:
         total += node_features[neighbor]
 
     count = 1 + len(adjacency[node])
-    updated_features[node] = total / count
+    updated_features[node] = total
 
 print("更新前")
 print(node_features)
